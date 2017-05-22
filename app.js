b846469a7a48185a19bf20f021edf989d06a2618
Iulian Tabara <iulian.tabara@heg.com>
2017-05-22 17:09:08 +0300

    Passing data and functions

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ new Vue({
   el:'#vue-app',
   data:{
     first_name: 'Iulian',
-    last_name: "Tabara"
+    last_name: 'Tabara'
+  },
+  methods: {
+    greet: function(time){
+      return 'Good ' + time + ', ' + this.first_name
+    }
   }
 });
