50e93ce3d009b781aea67ee392b15d172b2f4b59
Iulian Tabara <iulian.tabara@heg.com>
2017-05-23 12:58:06 +0300

    Adding keyboard events

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 new Vue({
   el:'#vue-app',
   data:{
-    first_name: 'Iulian',
-    last_name: 'Tabara',
-    website: 'https://www.iuliantabara.com',
-    websiteTag: '<a href="https://www.iuliantabara.com">iuliantabara.com</a>',
-    age:36,
-    x:0,
-    y:0
   },
   methods: {
-    greet: function(time){
-      return 'Good ' + time + ', ' + this.first_name
+    logName: function(){
+      console.log('you entered your name');
     },
-    addAge: function(inc){
-      this.age += inc;
-    },
-    subtractAge: function(dec){
-      this.age -= dec;
-    },
-    updateXY : function(event){
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click : function(){
-      alert("You clicked");
+    logAge: function(){
+      console.log('you entered your age');
     }
   }
 });
