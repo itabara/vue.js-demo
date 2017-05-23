e5f17ec966db8602fe0ce8d6bdcb340403ef5e1a
Iulian Tabara <iulian.tabara@heg.com>
2017-05-23 11:23:28 +0300

    Adding events

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
     first_name: 'Iulian',
     last_name: 'Tabara',
     website: 'https://www.iuliantabara.com',
-    websiteTag: '<a href="https://www.iuliantabara.com">iuliantabara.com</a>'
+    websiteTag: '<a href="https://www.iuliantabara.com">iuliantabara.com</a>',
+    age:36,
+    x:0,
+    y:0
   },
   methods: {
     greet: function(time){
       return 'Good ' + time + ', ' + this.first_name
+    },
+    addAge: function(inc){
+      this.age += inc;
+    },
+    subtractAge: function(dec){
+      this.age -= dec;
+    },
+    updateXY : function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
